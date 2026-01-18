d565bf6e2915fbfb650dc915af7c6ad42aba6d8b
Gadlee <bizimana.williamgad@students.mak.ac.ug>
2026-01-18 21:41:15 +0300

    final changes

---
+++ script.js
@@ -0,0 +1,13 @@
+// Smooth scrolling for internal links
+document.addEventListener("DOMContentLoaded", () => {
+    const links = document.querySelectorAll("a[href^='#']");
+    for (let link of links) {
+        link.addEventListener("click", function(e) {
+            e.preventDefault();
+            const target = document.querySelector(this.getAttribute("href"));
+            if (target) {
+                target.scrollIntoView({ behavior: "smooth" });
+            }
+        });
+    }
+});
